52bb405a51d5ef7bfa4f52014c852067c1201538
beatrizsiqueiras <beatrizsiqueiracosta2001@hotmail.com>
2024-06-11 20:43:52 -0300

    feat: página de listagem de alunos finalizada

--- src/app/components/student-list/student-list.component.ts
+++ src/app/components/student-list/student-list.component.ts
@@ -1,49 +1,16 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
-import { MatSort, MatSortModule } from '@angular/material/sort';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { MatInputModule } from '@angular/material/input';
+import { AfterViewInit, Component, ViewChild, OnInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
-
-export interface UserData {
-  id: string;
-  name: string;
-  progress: string;
-  fruit: string;
-}
-
-/** Constants used to fill up our data base. */
-const FRUITS: string[] = [
-  'blueberry',
-  'lychee',
-  'kiwi',
-  'mango',
-  'peach',
-  'lime',
-  'pomegranate',
-  'pineapple',
-];
-const NAMES: string[] = [
-  'Maia',
-  'Asher',
-  'Olivia',
-  'Atticus',
-  'Amelia',
-  'Jack',
-  'Charlotte',
-  'Theodore',
-  'Isla',
-  'Oliver',
-  'Isabella',
-  'Jasper',
-  'Cora',
-  'Levi',
-  'Violet',
-  'Arthur',
-  'Mia',
-  'Thomas',
-  'Elizabeth',
-];
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { ApiService } from 'src/app/services/api/api.service';
+import { ToastrService } from 'ngx-toastr';
+import { HttpClientModule } from '@angular/common/http';
+import { Student } from 'src/app/models/student.model';
 
 @Component({
   selector: 'app-student-list',
@@ -56,47 +23,58 @@ const NAMES: string[] = [
     MatTableModule,
     MatSortModule,
     MatPaginatorModule,
+    HttpClientModule,
   ],
 })
-export class StudentListComponent implements AfterViewInit {
-  displayedColumns: string[] = ['id', 'name', 'course', 'fruit'];
-  dataSource: MatTableDataSource<UserData>;
+export class StudentListComponent implements AfterViewInit, OnInit {
+  displayedColumns: string[] = ['id', 'name', 'course', 'actions'];
+  studentData: MatTableDataSource<Student>;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor() {
-    const users = Array.from({ length: 100 }, (_, k) => createNewUser(k + 1));
-    this.dataSource = new MatTableDataSource(users);
+  constructor(private toastr: ToastrService, private api: ApiService) {
+    this.studentData = new MatTableDataSource();
+  }
+
+  ngOnInit() {
+    this.api.get().subscribe({
+      next: (students: Student[]) => {
+        this.studentData.data = students;
+      },
+      error: (err: any) => {
+        this.toastr.error('Erro ao carregar os alunos.');
+      },
+    });
   }
 
   ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+    this.studentData.paginator = this.paginator;
+    this.studentData.sort = this.sort;
   }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+    this.studentData.filter = filterValue.trim().toLowerCase();
 
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
+    if (this.studentData.paginator) {
+      this.studentData.paginator.firstPage();
     }
   }
-}
 
-/** Builds and returns a new User. */
-function createNewUser(id: number): UserData {
-  const name =
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))] +
-    ' ' +
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) +
-    '.';
+  toggleActive(id: string) {
+    // @ts-ignore
+    const student = this.studentData.data.find((s) => s.id === id);
+    if (student) {
+      // @ts-ignore
+      student.active = !student.active;
+      this.toastr.success(
+        `Student ${student.active ? 'activated' : 'deactivated'}`
+      );
+    }
+  }
 
-  return {
-    id: id.toString(),
-    name: name,
-    progress: Math.round(Math.random() * 100).toString(),
-    fruit: FRUITS[Math.round(Math.random() * (FRUITS.length - 1))],
-  };
+  editStudent(id: string) {
+    this.toastr.info(`Edit student with id ${id}`);
+  }
 }
