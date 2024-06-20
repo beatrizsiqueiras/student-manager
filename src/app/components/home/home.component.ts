f95206549cc93e410f145ab13347bdffab5bec72
beatrizsiqueiras <beatrizsiqueiracosta2001@hotmail.com>
2024-06-20 20:51:04 -0300

    feat: odeio git

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [MatCardModule],
+    imports: [MatCardModule, MatDividerModule, MatListModule],
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css'],
 })
